7ceb3d3e693ce6822ab6dee3a4a0bd6d6d34eed1
Batel Shkolnik <batelshk@post.bgu.ac.il>
2021-07-11 00:11:43 +0300

    Added custom list

---
+++ app/src/main/java/com/example/bigvu_app/CustomItemList.java
@@ -0,0 +1,43 @@
+package com.example.bigvu_app;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomItemList extends ArrayAdapter {
+    private ArrayList<String> name;
+    private ArrayList<String> description;
+    private ArrayList<ImageView> imageid;
+    private Activity context;
+
+    public CustomItemList(Activity context, ArrayList<String> name, ArrayList<String> description, ArrayList<ImageView> imageid) {
+        super(context, R.layout.row_item, name);
+        this.context = context;
+        this.name = name;
+        this.description = description;
+        this.imageid = imageid;
+
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row=convertView;
+        LayoutInflater inflater = context.getLayoutInflater();
+        if(convertView==null)
+            row = inflater.inflate(R.layout.row_item, null, true);
+        TextView textViewCountry = (TextView) row.findViewById(R.id.textViewName);
+        TextView textViewCapital = (TextView) row.findViewById(R.id.textviewDesc);
+        ImageView imageFlag = (ImageView) row.findViewById(R.id.person_image);
+
+        textViewCountry.setText(name.get(position));
+        textViewCapital.setText(description.get(position));
+        imageFlag = imageid.get(position);
+        return  row;
+    }
+}
+
